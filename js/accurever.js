340783104c10148dba13bf0022140d5a84713e1e
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2021-10-28 02:02:44 +0800

    0.0.1 update

--- js/accurever.js
+++ js/accurever.js
@@ -1,7 +1,21 @@
-OnClrBtnShow = () => {
+OnClear = () => {
+    const getText = document.getElementById("yr-text");
+    const getResult = document.getElementById("yr-result");
 
+    getText.value = '';
+    getResult.innerHTML = '';
+}
+
+OnClrBtnShow = () => {
+    
 }
 
 OnReverse = () => {
-
+    const getText = document.getElementById("yr-text").value;
+    const getResult = document.getElementById("yr-result");
+    
+    for(r=getText.length-1; r > -1; r--) {
+        const makeChildOnResult = document.createTextNode(getText[r]);
+        getResult.appendChild(makeChildOnResult);
+    }
 }
