f84046216bbaafba2431e581d95d1e56a4af13c3
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2021-10-28 23:26:46 +0800

    Replacing some codes that I finished.

--- js/accurever.js
+++ js/accurever.js
@@ -1,21 +1,51 @@
 OnClear = () => {
-    getText = document.getElementById("yr-text");
-    getResult = document.getElementById("yr-result");
-
-    getText.value = '';
-    getResult.innerHTML = '';
+	getText = document.getElementById("yr-text");
+	getClrBtn = document.getElementById("yr-clear");
+	getResult = document.getElementById("yr-result");
+	
+	getText.value = '';
+	getResult.innerHTML = '...';
+	
+	getClrBtn.style.opacity = '0%';
 }
 
-OnClrBtnShow = () => {
-    
+OnEnterColor = () => {
+	getLay = document.getElementById("yr-root");
+	getHangerText = document.getElementById("yr-hanger-text").value;
+	setHanger = document.getElementById("yr-hanger");
+	
+	getLay.style.backgroundColor = getHangerText;
+	setHanger.style.animation = 'FadeOut';
+	setHanger.style.animationDuration = '0.3s';
+	setHanger.style.display = 'none';
 }
 
-OnReverse = () => {
-    getText = document.getElementById("yr-text").value;
-    getResult = document.getElementById("yr-result");
-    
-    for(r=getText.length-1; r > -1; r--) {
-        makeChildOnResult = document.createTextNode(getText[r]);
-        getResult.appendChild(makeChildOnResult);
-    }
+OnResult = () => {
+	getText = document.getElementById("yr-text").value;
+	
+	p = document.getElementById("yr-result");
+	getText == '' ? p.innerHTML = "Oops! you need to put something." : p.innerHTML = '';
+	for(r=getText.length-1; r > -1; r--) {
+		pData = document.createTextNode(getText[r]); 
+		p.appendChild(pData); 
+	}
+}
+
+OnShowClearBtn = () => {
+	getLay = document.getElementById("yr-root");
+	getText = document.getElementById("yr-text").value;
+	setText = document.getElementById("yr-text");
+	setCurTime = document.getElementById("yr-time");
+	getClrBtn = document.getElementById("yr-clear");
+	setHanger = document.getElementById("yr-hanger");
+	setHanger1 = document.getElementById("yr-hanger1");
+	
+	getText == '' ? getClrBtn.style.opacity = '0%' : getClrBtn.style.opacity = '100%';
+	
+	if(getText == 'flanx::set://background_color') {
+		setHanger.style.display = 'flex';
+		setHanger.style.animation = 'FadeIn';
+		setHanger.style.animationDuration = '0.3s';
+		setText.value = '';
+	}
 }
