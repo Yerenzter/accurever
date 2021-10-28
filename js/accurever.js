c6ba9d49ae17730deb0370670acc0f1843f84caf
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2021-10-29 00:27:35 +0800

    Minor changes

--- js/accurever.js
+++ js/accurever.js
@@ -10,7 +10,7 @@ OnClear = () => {
 }
 
 OnEnterColor = () => {
-	getLay = document.getElementsByTagName("body");
+	getLay = document.getElementsById("yr-root");
 	getHangerText = document.getElementById("yr-hanger-text").value;
 	setHanger = document.getElementById("yr-hanger");
 	
